2093e4b85715d99bfe08f4f1459ec0d848099fca
Isaac Ezer <isaac.ezer@gmail.com>
2012-07-07 14:38:00 +0900

    1st commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gigsite::Application.config.secret_token = 'b2af766c33e847a9e0a4d3a02a0c3fd0cc525187ee6ed4cd7f77abe1130e268c963fdf83f267ecd5437942ce2c0546219fd3801477fb92dbc032cba8ae4a9fad'
