0f0fbd2c97f8b38497983cef9e904e9628cf4fbb
Isaac Ezer <isaac.ezer@gmail.com>
2012-07-09 13:25:22 +0900

    added language link to every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale
   
-  private 
+  private
+  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
+
+  private   
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
